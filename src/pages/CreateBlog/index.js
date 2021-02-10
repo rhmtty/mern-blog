327bba79513a4a20f3e350e803f1e8a52632a49b
GrimmLck <rhmtofficial@gmail.com>
2021-02-10 12:05:07 +0700

    Implementasi Menambahkan Postingan baru & Upload Image

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Button, Gap, Input, Link, TextArea, Upload} from '../../components'
 import './createBlog.scss'
 import {useHistory} from 'react-router-dom'
+import Axios from 'axios'
 
 const CreateBlog = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [image, setImage] = useState('');
+    const [imagePreview, setImagePreview] = useState(null);
     const history = useHistory()
+
+    const onSubmit = () => {
+        console.log('title: ', title);
+        console.log('body: ', body);
+        console.log('image: ', image);
+
+        const data = new FormData();
+        data.append('title', title);
+        data.append('body', body)
+        data.append('image', image)
+
+        Axios.post('http://localhost:4000/v1/blog/post', data, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+        .then(res => {
+            console.log('post success: ', res);
+        })
+        .catch(err => {
+            console.log('err: ', err);
+        })
+    }
+
+    const onImgaeUpload = (event) => {
+        const file = event.target.files[0]
+        setImage(file)
+        setImagePreview(URL.createObjectURL(file))
+    }
     return (
         <div className="blog-post">
             <Link title="kembali" onClick={() => history.push('/')} />
             <p className="title">Create New Blog Post</p>
-            <Input label="Post Title" />
-            <Upload />
-            <TextArea />
+            <Input label="Post Title" value={title} onChange={(event) => setTitle(event.target.value)} />
+            <Upload onChange={event => onImgaeUpload(event)} img={imagePreview} />
+            <TextArea value={body} onChange={event => setBody(event.target.value)} />
             <Gap height={20} />
             <div className="button-action">
-                <Button title="save" />
+                <Button title="Save" onClick={onSubmit} />
             </div>
             <Gap height={60} />
         </div>
