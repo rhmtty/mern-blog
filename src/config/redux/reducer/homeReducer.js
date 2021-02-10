9d6c75a9e2cef2e33ff068d55d94904a46d54a34
GrimmLck <rhmtofficial@gmail.com>
2021-02-10 10:45:10 +0700

    Implementasi Pagination di halaman homepage

--- src/config/redux/reducer/homeReducer.js
+++ src/config/redux/reducer/homeReducer.js
@@ -1,12 +1,23 @@
 const initialStateHome = {
     dataBlog: [],
+    page: {
+        currentPage: 1,
+        perPage: 1
+    }
 };
 
 const homeReducer = (state = initialStateHome, action) => {
-    if(action.type === 'UPADATE_DATA_BLOG') {
+    if(action.type === 'UPDATE_DATA_BLOG') {
         return {
             ...state,
             dataBlog: action.payload
+        }
+    }
+
+    if(action.type === 'UPDATE_PAGE') {
+        return {
+            ...state,
+            page: action.payload
         }
     }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {BlogItem, Button, Gap} from '../../components'
 import './home.scss'
 import {useHistory} from 'react-router-dom'
@@ -6,13 +6,24 @@ import { useDispatch, useSelector } from 'react-redux'
 import { setDataBlog } from '../../config/redux/action'
 
 const Home = () => {
-    const {dataBlog} = useSelector(state => state.homeReducer);
+    const [counter, setCounter] = useState(1)
+    const {dataBlog, page} = useSelector(state => state.homeReducer);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(setDataBlog())
-    }, [dispatch])
+        dispatch(setDataBlog(counter))
+    }, [counter, dispatch])
+
     const history = useHistory()
+
+    const previous = () => {
+        setCounter(counter <= 1 ? 1 : counter - 1)
+    }
+
+    const next = () => {
+        setCounter(counter === page.totalPage ? page.totalPage : counter + 1)
+    }
+
     return (
         <div className="home-page-wrapper">
             <div className="create-wrapper">
@@ -34,13 +45,15 @@ const Home = () => {
                 })}
             </div>
             <div className="pagination">
-                <Button title="previous"/>
+                <Button title="previous" onClick={previous} />
                 <Gap width={20} />
-                <Button title="next"/>
+                <p className="text-page">{page.currentPage} / {page.totalPage}</p>
+                <Gap width={20} />
+                <Button title="next" onClick={next} />
             </div>
             <Gap height={20} />
         </div>
     )
 }
 
-export default Home
+export default Home;
