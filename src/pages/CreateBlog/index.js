7b858d8f36a0933c242ed497b5bac1d0b1374b9e
GrimmLck <rhmtofficial@gmail.com>
2021-02-13 15:18:58 +0700

    Implementasi Update Blog Post

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Button, Gap, Input, Link, TextArea, Upload} from '../../components'
 import './createBlog.scss'
-import {useHistory} from 'react-router-dom'
+import {useHistory, withRouter} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { postToAPI, setForm, setImgPreview } from '../../config/redux/action'
+import { postToAPI, setForm, setImgPreview, updateToAPI } from '../../config/redux/action'
+import Axios from 'axios'
 
-const CreateBlog = () => {
+const CreateBlog = (props) => {
     const {form, imgPreview} = useSelector(state => state.createBlogReducer);
     const {title, body} = form;
+    const [isUpdate, setIsUpdate] = useState(false);
     const dispatch = useDispatch()
     const history = useHistory()
 
+    useEffect(() => {
+        const id = props.match.params.id;
+        console.log('params: ', props.match.params.id);
+        if(id){
+            setIsUpdate(true);
+            Axios.get(`http://localhost:4000/v1/blog/post/${id}`)
+            .then(res => {
+                const data = res.data.data;
+                console.log('res: ', data);
+                dispatch(setForm('title', data.title));
+                dispatch(setForm('body', data.body));
+                dispatch(setImgPreview(`http://localhost:4000/${data.image}`));
+            })
+            .catch(err => {
+                console.log('err: ', err);
+            })
+        }
+    }, [props, dispatch])
+
     const onSubmit = () => {
-        postToAPI(form)
+        const id = props.match.params.id;
+        if(isUpdate){
+            console.log('update data');
+            updateToAPI(form, id)
+        } else{
+            console.log('create data');
+            postToAPI(form)
+        }
     }
 
     const onImgaeUpload = (event) => {
@@ -23,17 +51,17 @@ const CreateBlog = () => {
     return (
         <div className="blog-post">
             <Link title="kembali" onClick={() => history.push('/')} />
-            <p className="title">Create New Blog Post</p>
+            <p className="title">{isUpdate ? 'Update' : 'Create New'} Blog Post</p>
             <Input label="Post Title" value={title} onChange={(event) => dispatch(setForm('title', event.target.value))} />
             <Upload onChange={event => onImgaeUpload(event)} img={imgPreview} />
             <TextArea value={body} onChange={event => dispatch(setForm('body', event.target.value))} />
             <Gap height={20} />
             <div className="button-action">
-                <Button title="Save" onClick={onSubmit} />
+                <Button title={isUpdate ? 'Update' : 'Simpan'} onClick={onSubmit} />
             </div>
             <Gap height={60} />
         </div>
     )
 }
 
-export default CreateBlog
+export default withRouter(CreateBlog)
