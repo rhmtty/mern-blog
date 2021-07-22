ad69c28eae2bc92a964678899d6ca66a26076342
GrimmLck <rhmtofficial@gmail.com>
2021-07-22 15:29:46 +0700

    Deploy to Heroku!

---
+++ build/precache-manifest.4ad93b9c2e9920c6f23f3fe81e9a28e4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d33f6a7cc976d41ec071faa27285874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5988541e7b3c254a0d4",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "fd23f36ca8dafa541a5e",
+    "url": "/static/css/main.168c386e.chunk.css"
+  },
+  {
+    "revision": "f5988541e7b3c254a0d4",
+    "url": "/static/js/2.719acc30.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.719acc30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd23f36ca8dafa541a5e",
+    "url": "/static/js/main.b7fbc3f0.chunk.js"
+  },
+  {
+    "revision": "f32fa013b6d79392388e",
+    "url": "/static/js/runtime-main.3edc87a7.js"
+  },
+  {
+    "revision": "7efd3b80b21a4ce3ab7188f36a93aa5a",
+    "url": "/static/media/facebook.7efd3b80.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "e99a71c6f26d796a7e87e70687200829",
+    "url": "/static/media/instagram.e99a71c6.svg"
+  },
+  {
+    "revision": "0f01303114d33e00c77f44bcc660c112",
+    "url": "/static/media/loginBG.0f013031.jpg"
+  },
+  {
+    "revision": "e93b05472750de11e12da888aa76751f",
+    "url": "/static/media/registerBG.e93b0547.jpg"
+  },
+  {
+    "revision": "4c49693b4625bab003b51956f3ce29bf",
+    "url": "/static/media/telegram.4c49693b.svg"
+  }
+]);
